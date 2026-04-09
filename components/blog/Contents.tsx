2fdd28c4500f0aa4ec3b84370c60eb45d46f7716
tonymac129 <tonyforself@gmail.com>
2026-04-08 21:16:43 -0400

    Fix deployment

--- components/blog/Contents.tsx
+++ components/blog/Contents.tsx
@@ -6,7 +6,7 @@ function Contents({ post }: { post: PostType }) {
     <div className="flex flex-col gap-y-5 py-5 w-45 border-l-2 border-l-zinc-800 pl-3 sticky top-18.5 h-[calc(100vh-74px)]">
       <h2 className="text-white text-lg font-bold px-4">Post Contents</h2>
       <div className="flex flex-col gap-y-3">
-        {post.headings.map((heading) => (
+        {post.headings!.map((heading) => (
           <Link
             key={heading.id}
             href={`/blog/${post.slug}/#${heading.id}`}
